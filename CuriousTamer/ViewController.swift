0ecefad889e92f2b78ca36d20fb35099b6193dd2
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2015-12-16 18:11:23 +0200

    just opened the project.

---
+++ CuriousTamer/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CuriousTamer
+//
+//  Created by Seda Nur Doganay on 16/12/15.
+//  Copyright © 2015 Seda Nur Doganay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
