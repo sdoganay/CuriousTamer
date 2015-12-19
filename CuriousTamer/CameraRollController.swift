62052e69192a3f406d3d64acd6088ffbcefc8ab6
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2015-12-19 23:47:20 +0200

    controller classes

---
+++ CuriousTamer/CameraRollController.swift
@@ -0,0 +1,24 @@
+//
+//  CameraRollController.swift
+//  CuriousTamer
+//
+//  Created by Seda Nur Doganay on 19/12/15.
+//  Copyright © 2015 Seda Nur Doganay. All rights reserved.
+//
+
+import UIKit
+
+class CameraRollController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ CuriousTamer/ImageDetailController.swift
@@ -0,0 +1,9 @@
+//
+//  ImageDetailController.swift
+//  CuriousTamer
+//
+//  Created by Seda Nur Doganay on 19/12/15.
+//  Copyright © 2015 Seda Nur Doganay. All rights reserved.
+//
+
+import Foundation
