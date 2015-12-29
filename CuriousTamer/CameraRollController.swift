95df019dbf828c901611d95d7ed265656c7a70da
Seda Nur Doganay <seda.doganay@ozu.edu.tr>
2015-12-29 14:04:33 +0200

    --

--- CuriousTamer/CameraRollController.swift
+++ CuriousTamer/CameraRollController.swift
@@ -13,12 +13,11 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
     
     var captureSession : AVCaptureSession?
     var stillImageOutput : AVCaptureStillImageOutput?
-
+    
     var audioPlayer = AVAudioPlayer()
     var leftSwipe : UISwipeGestureRecognizer!
     var rightSwipe : UISwipeGestureRecognizer!
     
-
     
     @IBOutlet weak var chooseButton: UIButton!
     
@@ -41,16 +40,62 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
     func handleSwipes(sender: UISwipeGestureRecognizer){
         imageView.hidden = true
         prepareScaryGif()
-        prepareScarySound()
+        prepareScarySound("scaryScream")
         
         //Gif to be seen.
         self.view.addSubview(webViewBG)
-        
-        //Sound to be heard.
         audioPlayer.play()
-        captureImage()
+        //Sound to be heard.
+        
+        
+        let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 3 * Int64(NSEC_PER_SEC))
+        dispatch_after(time, dispatch_get_main_queue()) {
+            //put your code which should be executed with a delay here
+            self.captureImage()
+            
+            let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 1 * Int64(NSEC_PER_SEC))
+            dispatch_after(time, dispatch_get_main_queue()) {
+                self.webViewBG.hidden = true
+                self.imageView.hidden = false
+                self.prepareScarySound("evilLaugh")
+                self.audioPlayer.play()
+                self.popUpBustedMessage()
+                print("BUSTED")
+            }
+        }
+    }
+    
+    
+    func popUpBustedMessage(){
+        let alert = UIAlertController(title: "YOU ARE BUSTED!", message: "What a shame! You were peeping some photos...", preferredStyle: UIAlertControllerStyle.Alert)
+        alert.addAction(UIAlertAction(title: "OK, you got me!", style: UIAlertActionStyle.Default, handler: {
+            action in
+            switch action.style{
+            case .Default:
+                print("default")
+                let time = dispatch_time(dispatch_time_t(DISPATCH_TIME_NOW), 4 * Int64(NSEC_PER_SEC))
+                dispatch_after(time, dispatch_get_main_queue()) {
+                    //ekran karart
+                    self.imageView.image = UIImage(named: "youFinger.png")
+                    
+                    //swipePassword
+                    print("swipePassword yerindeyim!")
+                    
+                }
+                
+            case .Cancel:
+                print("cancel")
+            case .Destructive:
+                print("destructive")
+            }
+        }))
+
+        self.presentViewController(alert, animated: true, completion: nil)
+        
         
     }
+    
+    
     override func viewWillAppear(animated: Bool) {
         
         super.viewWillAppear(animated)
@@ -67,6 +112,7 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
                 break
             }
         }
+        
         var input : AVCaptureDeviceInput?
         do{
             input = try AVCaptureDeviceInput(device: captureDevice)}catch{}
@@ -88,52 +134,23 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
     
     
     func captureImage(){
-        //        let imagePickerFromCamera = UIImagePickerController()
-        //        imagePickerFromCamera.delegate = self
-        //
-        //        if(UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)){
-        //            imagePickerFromCamera.sourceType = UIImagePickerControllerSourceType.Camera
-        //        }else{
-        //            print("No camera available!")
-        //        }
-        //        presentViewController(imagePickerFromCamera, animated: true, completion: nil)
         if let videoConnection = stillImageOutput?.connectionWithMediaType(AVMediaTypeVideo){
             videoConnection.videoOrientation = AVCaptureVideoOrientation.Portrait
             stillImageOutput?.captureStillImageAsynchronouslyFromConnection(videoConnection, completionHandler: { (sampleBuffer, error) -> Void in
                 if(sampleBuffer != nil ){
-                    var imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
-                    var dataProvider = CGDataProviderCreateWithCFData(imageData)
-                    var cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true,CGColorRenderingIntent.RenderingIntentDefault)
-                    var image = UIImage(CGImage: cgImageRef!,scale: 1.0, orientation: UIImageOrientation.Right)
+                    let imageData = AVCaptureStillImageOutput.jpegStillImageNSDataRepresentation(sampleBuffer)
+                    let dataProvider = CGDataProviderCreateWithCFData(imageData)
+                    let cgImageRef = CGImageCreateWithJPEGDataProvider(dataProvider, nil, true,CGColorRenderingIntent.RenderingIntentDefault)
+                    let image = UIImage(CGImage: cgImageRef!,scale: 1.0, orientation: UIImageOrientation.Right)
                     print("snaap! :) ")
+                    
                     self.imageView.image = image
-                    self.imageView.hidden = false
-                    
-                    let date = NSDate()
-                    let calendar = NSCalendar.currentCalendar()
-                   let components = calendar.components(NSCalendarUnit.Year.union(NSCalendarUnit.Minute), fromDate: date)
-                    let hour = components.hour
-                    let minutes = components.minute
-                    let day = date.description
-                   print("Scared_\(day)-\(hour):\(minutes).jpeg is writing..")
-//                    if let data = UIImagePNGRepresentation(image) {
-//                        let filename = self.getDocumentsDirectory().stringByAppendingPathComponent("Scared_\(day)-\(hour):\(minutes).png")
-//                        data.writeToFile(filename, atomically: true)
-//                    }
-                    
-//                    if let data = UIImageJPEGRepresentation(image, 0.8) {
-//                            let filename = self.getDocumentsDirectory().stringByAppendingPathComponent("Scared_\(day)-\(hour):\(minutes).jpeg")
-//                        data.writeToFile(filename, atomically: true)
-//                        print("file is done!")
-//                    }
-                    
-                     UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
+                    UIImageWriteToSavedPhotosAlbum(image, nil, nil, nil)
                     
                     print("DONE!")
                 }
             })
         }
-        
         
         
     }
@@ -145,27 +162,22 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
         return documentsDirectory
     }
     
-    //    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
-    //        imageView.image = info[UIImagePickerControllerOriginalImage] as? UIImage; dismissViewControllerAnimated(true, completion: nil)
-    //    }
-    
     func prepareScaryGif(){
         let filePath = NSBundle.mainBundle().pathForResource("scarryClown", ofType: ".gif")
         let scarryGif = NSData(contentsOfFile: filePath!)
-        //        webViewBG.backgroundColor = UIColor.blackColor()
         webViewBG.loadData(scarryGif!, MIMEType: "image/gif", textEncodingName: String(), baseURL: NSURL())
         webViewBG.userInteractionEnabled = false;
     }
     
-    func prepareScarySound(){
-        let scarySound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource("scaryScream", ofType: "mp3")!)
+    func prepareScarySound(soundName: String){
+        let scarySound = NSURL(fileURLWithPath: NSBundle.mainBundle().pathForResource(soundName, ofType: "mp3")!)
         do{
             try AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback)
             try AVAudioSession.sharedInstance().setActive(true)
             try audioPlayer = AVAudioPlayer(contentsOfURL: scarySound, fileTypeHint: nil)
             audioPlayer.prepareToPlay()
         }catch{
-            print("probleeem")
+            print("AVAudioSession problem occured.")
         }
         
     }
@@ -176,24 +188,23 @@ class CameraRollController: UIViewController, UIImagePickerControllerDelegate, U
         imagePickerFromLibrary.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         presentViewController(imagePickerFromLibrary, animated: true, completion: nil)
     }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingImage image: UIImage, editingInfo: [String : AnyObject]?) {
         chooseButton.hidden = true
         imageView.image = image
         imageView.contentMode = .ScaleAspectFit
         self.dismissViewControllerAnimated(true, completion: nil)
-        //        view.backgroundColor = UIColor.blackColor()
         
     }
     
     @IBAction func pinchResponder(sender: AnyObject) {
         //zoom here!
         print("zoom zOOm")
+        
     }
     
     
